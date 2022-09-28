a0c316f5ec90054a94ba39dd24b1e89bc6044188
Indonode.dev <34649601+elangrr@users.noreply.github.com>
2022-09-28 16:02:02 +0700

    Update Testnet.js

--- src/components/Testnet.js
+++ src/components/Testnet.js
@@ -129,6 +129,7 @@ export const Testnet = () => {
                             <a href="https://github.com/elangrr/testnet_manuals/tree/main/rebus" target="_blank"><img src={meter19} alt="Image" /></a>
                             <a href="https://github.com/elangrr/testnet_manuals/tree/main/rebus" target="_blank"><h5>Rebus</h5></a>
                              <div className="item">
+                            </div>
                             <a href="https://github.com/elangrr/ICW_GUIDE" target="_blank"><img src={meter21} alt="Image" /></a>
                             <a href="https://github.com/elangrr/ICW_GUIDE" target="_blank"><h5>ICW CHAIN</h5></a>
                             </div>
